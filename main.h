a18a48f081b663837e794ad6ddbfd4882e4f9cd3
muro906 <mainamillicent68@gmail.com>
2023-08-25 12:32:59 +0300

    Tasks done

---
+++ main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include<limits.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<stdarg.h>
+/**
+ * struct format - match conversion specifiers for printf
+ * @specifier: Type of char. Pointer to conversion specifier
+ * @func_print: pointer to function that handles the conversion specifier
+*/
+typedef struct format{
+    char *specifier;
+    int (*func_print)(va_list args);/*Functions to handle the following specifiers: %s,%c, %%,%d,%ld,%i,%b,%u,%o,%x,%X,%s,%S*/
+}convert_match;
+/*Functions to handle the %c, %s, %% specifiers*/
+int _putchar(char c);
+int _puts(va_list args);
+int _strlen(const char *s);
+char *_strrev(char *s);
+int print_char(va_list args);
+#endif /**MAIN_H*/
---
+++ print_case.c
@@ -0,0 +1,5 @@
+#include"main.h"
+/**
+ * print_case-Handles the conversion specifiers
+ * @format
+*/
